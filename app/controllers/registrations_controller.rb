42ecf601c6dda056b6bbc34b0e1473e502ef9e6d
mooncake <adom.donatas@gmail.com>
2024-10-09 20:15:08 +0300

    feat: add sign in and sign up links with correct visibility

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,8 +1,10 @@
 class RegistrationsController < ApplicationController
   allow_unauthenticated_access only: [:new, :create]
+  before_action :resume_session, only: [:new]
   rate_limit to: 10, within: 3.minutes, only: :create, with: -> { redirect_to new_registration_url, alert: "Try again later." }
 
   def new
+    redirect_to root_url if authenticated?
     @user = User.new
   end
 
